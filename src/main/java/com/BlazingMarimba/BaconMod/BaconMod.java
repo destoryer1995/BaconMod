31d34918b33f9ba5f74cb1058073ffd059345e8d
Micahel <mlb199527@gmail.com>
2014-09-02 16:17:59 -0700

    Initial Commit

---
+++ src/main/java/com/BlazingMarimba/BaconMod/BaconMod.java
@@ -0,0 +1,31 @@
+package com.BlazingMarimba.BaconMod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "BaconMod", name = "Bacon Mod", version = "1.7.10-1.0")
+public class BaconMod {
+
+    @Mod.Instance("BaconMod")
+    public static BaconMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
