d821d1078b5619a04eecd0af42fb4fcae9c2445c
Micahel <mlb199527@gmail.com>
2014-09-02 16:27:58 -0700

    Add utilty and reference class

---
+++ src/main/java/com/BlazingMarimba/BaconMod/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.BlazingMarimba.BaconMod.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "BaconMod";
+    public static final String MOD_NAME = "Bacon Mod";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String Client_Proxy = "com.BlazingMarimba.BaconMod.Proxy.ClientProxy";
+    public static final String Server_Proxy = "com.BlazingMarimba.BaconMod.Proxy.ServerProxy";
+    public static final String GUI_FACTORY = "com.BlazingMarimba.BaconMod.client.gui.GuiFactory";
+}
