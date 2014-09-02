992db50f5ef202186f9e9402746000762419b03f
Micahel <mlb199527@gmail.com>
2014-09-02 16:28:34 -0700

    Added ulilty and reference methods

--- src/main/java/com/BlazingMarimba/BaconMod/BaconMod.java
+++ src/main/java/com/BlazingMarimba/BaconMod/BaconMod.java
@@ -1,11 +1,13 @@
 package com.BlazingMarimba.BaconMod;
 
+import com.BlazingMarimba.BaconMod.reference.Reference;
+import com.BlazingMarimba.BaconMod.utilty.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "BaconMod", name = "Bacon Mod", version = "1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class BaconMod {
 
     @Mod.Instance("BaconMod")
@@ -14,18 +16,18 @@ public class BaconMod {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void Init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete");
     }
 }
